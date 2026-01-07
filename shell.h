148aeb285e8ffaa99e3e8a42cd4f9f0affc0d324
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-07 17:37:33 +0100

    feat:Addedstructure of list_t for the get_path and  added _getenv get_path prototype

--- shell.h
+++ shell.h
@@ -3,9 +3,29 @@
 
 #include <unistd.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 /* string.c */
 char *str_concat(char *s1, char *s2);
 
+/* env.c */
+char *_getenv(const char *name);
+list_t *get_path();
+
+/* exec.c */
 int pre_exec(char **args);
 
 #endif
