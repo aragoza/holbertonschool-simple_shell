fdf8e26d2d18317c282a6414770c14d5d9873bed
aragoza <charlet.elliot@gmail.com>
2026-01-07 18:50:36 +0100

    the programm is executing the command and his argument for the cmd pass in parameter

--- shell.h
+++ shell.h
@@ -1,6 +1,38 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+extern char **environ;
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* string.c */
+char *str_concat(char *s1, char *s2);
+
+/* env.c */
+char *_getenv(const char *name);
+list_t *get_path();
+
+/* exec.c */
+int pre_exec(char **args);
+
+/** _executee_arg.c */
+void execute_args(char **args);
 
 #endif
