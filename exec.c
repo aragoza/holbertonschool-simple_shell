2b78bd280aee7a6795a9a894d960cedb39cc89ea
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-08 22:19:00 +0100

    fix: Added some comments Oo

--- exec.c
+++ exec.c
@@ -17,41 +17,38 @@ char *get_bin_path(char **args)
 	list_t *head, *path;
 	char *path_str, *new_bin_path;
 
-	new_bin_path = str_concat("/bin/", args[0]);
-	if (access(new_bin_path, X_OK) == 0)
-	{
+	if (access(args[0], X_OK) == 0) /* If command is a bin*/
+		return (args[0]); /* Return the bin given by user*/
+	new_bin_path = str_concat("/bin/", args[0]);/* Concat /bin + command*/
+	if (access(new_bin_path, X_OK) == 0)/* Check if /bin + command is executable */
 		return (new_bin_path);
-	}
 	free(new_bin_path);
 
-	path = get_path();
-	head = path;
+	head = path = get_path(); /* Set path to get_path and copy it to head */
 	while (path != NULL)
 	{
 		if (path->str != NULL)
 		{
-			path_str = path->str;
-			path_len = _strlen(path_str);
+			path_str = path->str; /* Get the dir path */
+			path_len = _strlen(path_str); /* Get length of the dir path */
 
-			if (path_str[path_len - 1] != '/')
-				path_str = str_concat(path_str, "/");
-			new_bin_path = str_concat(path_str, args[0]);
+			if (path_str[path_len - 1] != '/') /* If path don't finish by / */
+				path_str = str_concat(path_str, "/"); /* Then add / at end of path_str */
+			new_bin_path = str_concat(path_str, args[0]);/*Concat dir path and command*/
 			free(path_str);
 
+			/* Check if the command is an executable in the dir of the path */
 			if (access(new_bin_path, X_OK) == 0)
-			{
-				break;
-			}
-			free(new_bin_path);
+				break; /* Stop search in path bc its found */
+			free(new_bin_path); /* Free new_bin_path of memory */
 		}
-		path = path->next;
+		path = path->next; /* Set path to the next path*/
 	}
 
-	free_list(head);
+	free_list(head); /* Free the path list*/
 	if (new_bin_path != NULL)
-		return (new_bin_path);
-
-	return ("");
+		return (new_bin_path); /* If path found then return path*/
+	return (NULL); /* If not found then return NULL*/
 }
 
 int execute_command(char **args)
@@ -62,13 +59,11 @@ int execute_command(char **args)
 
 	if (!args || !args[0])
 		return (-1);
+	if (check_and_execute_builtin(args) == 0) /* 1. Check built-ins first */
+		return (0); /* Successful executed*/
 
-	/* 1. Check built-ins first */
-	if (check_and_execute_builtin(args) == 0)
-		return (0); /* built-in executed successfully */
+	cmd_path = get_bin_path(args); /* 2. Search in /bin and all dirs of PATH */
 
-	/* 2. Search in /bin and all dirs of PATH */
-	cmd_path = get_bin_path(args);
 	if (cmd_path == NULL)
 	{
 		fprintf(stderr, "%s: command not found\n", args[0]);
@@ -90,9 +85,7 @@ int execute_command(char **args)
 		exit(1);
 	}
 	else /* parent process */
-	{
 		wait(&status);
-	}
 
 	free(cmd_path);
 	return (status);
