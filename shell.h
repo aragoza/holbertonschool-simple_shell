99d10caeeb545ff3cd191af18be5b5f4b5ba6c27
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-08 22:33:55 +0100

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- shell.h
+++ shell.h
@@ -9,6 +9,21 @@
 extern char **environ;
 
 /**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
  * struct builtin_s - function builtin
  * @name: name of the command
  * @func: function pointer
@@ -37,11 +52,10 @@ char *str_concat(char *s1, char *s2);
 
 /* env.c */
 char *_getenv(const char *name);
-char *get_path(char *name);
+list_t *get_path();
 
 /* free.c */
-/** void free_list(list_t *head);*/
+void free_list(list_t *head);
 void free_iteratively(char **argv);
-
 
 #endif /* SHELL_H */
