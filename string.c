556a4bd88654c253117684db1e99d5c409b4b251
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-08 23:40:36 +0100

    feat: Added _strcmp function
    Compare two strings

--- string.c
+++ string.c
@@ -83,3 +83,23 @@ char *_strdup(char *str)
 
 	return (result);
 }
+
+/**
+ * _strcmp - Compare two string
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 0 if strings is eqal
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	unsigned int i = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
