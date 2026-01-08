846648e3b9acca8d73c22f704d4b6f8017dfe805
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-08 23:17:26 +0100

    feat: Added _strdup & _strcmp prototype

--- shell.h
+++ shell.h
@@ -49,6 +49,8 @@ int execute_command(char **args);
 /* string.c */
 int _strlen(char *str);
 char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
 
 /* env.c */
 char *_getenv(const char *name);
