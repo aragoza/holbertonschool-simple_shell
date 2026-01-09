21ad37317f6bc6fcc70ece8a17a3a2176402d870
Robin73389 <12229@holbertonstudents.com>
2026-01-09 13:24:02 +0100

    Add the build-in env and exit

--- check_then_exec_builtin.c
+++ check_then_exec_builtin.c
@@ -1,18 +1,51 @@
 #include "shell.h"
 #include <string.h>
 
+extern char **environ;
+
+/**
+ * our_exit - This is the build-in of exit
+ *
+ * Return: 0
+ */
+
+int our_exit(void)
+{
+	exit(0);
+	return (0);
+}
+
+/**
+ * __env - This is the build-in of env
+ *
+ * Return: 0
+*/
+
+int __env(void)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	return (0);
+}
+
 /**
  * check_and_execute_builtin - check and execute builtin if find
  * @args: array of token passed in the stream
  *
  * Return: 0 on success, 1 on fail
- */
-
+*/
 
 int check_and_execute_builtin(char **args)
 {
 	int i;
 	builtin_t builtins[] = {
+		{"exit", our_exit},
+		{"env", __env},
 		{NULL, NULL}
 	};
 
